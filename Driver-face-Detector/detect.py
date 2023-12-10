d11b56974e2e2d55bcf67b90f589c410c9b8bb11
SallemiYessmine <sallemiyessmine@ieee.org>
2023-12-10 23:38:05 +0100

    first commit

---
+++ Driver-face-Detector/detect.py
@@ -0,0 +1,45 @@
+import cv2
+import time
+from playsound import playsound
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(not len(eyes)):
+            t = 0
+            while(not len(eyes)):
+                t += 1
+                if t > 2:   # driver is sleeping
+                    playsound("C:\\Users\\MSI\\Desktop\\Driver-Sleep-Detector\\alarme.mp3")
+                    print ("driver is sleeping! /n")
+                    cv2.putText(frame, "driver is sleeping", (5, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    
+                    time.sleep(2)
+                    
+                    break
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            #cv2.putText(frame, "driver is awake", (5, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
